29d589e670f269bf3d19dc66dfc6f1ce796a5c60
aykhansm <ayxan689@gmail.com>
2021-02-11 16:34:15 +0400

    dersde yazdigimiz tapsiriq

---
+++ 11022021/Book.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _11022021
+{
+    class Book
+    {
+        private string _name;
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value) && value.Length >= 3)
+                    _name = value;
+            }
+        }
+        private int _pagecount;
+        public int PageCount
+        {
+            get
+            {
+                return _pagecount;
+            }
+            set
+            {
+                if (value >= 10)
+                    _pagecount = value;
+            }
+        }
+        public Book(string name,int pagecount)
+        {
+            this.Name = name;
+            this.PageCount = pagecount;
+        }
+    }
+}
---
+++ 11022021/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+namespace _11022021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Book> books = new List<Book>();
+            string name;
+            string pagecountStr;
+            int pagecount;
+            
+            EnterAgain:
+            EnterNameCorrectly:
+                Console.Write("Kitab adi daxil edin: ");
+                name = Console.ReadLine();
+                name = name.Trim();
+            
+            if (name.Length < 3)
+                goto EnterNameCorrectly;
+
+            bool CanConvert;
+            do
+            {
+                Console.Write("Kitabin sehife sayini daxil edin: ");
+                pagecountStr = Console.ReadLine();
+                CanConvert = int.TryParse(pagecountStr, out pagecount);
+                if (!CanConvert)
+                    Console.WriteLine("Duzgun daxil edin!");
+                
+                else if (pagecount < 10)
+                    Console.WriteLine("Sehife sayi 10 dan kicik ola bilmez!");
+            
+            } while (!CanConvert || pagecount < 10);
+
+            
+            
+            Book newbook = new Book(name, pagecount);
+            books.Add(newbook);
+            string YorN;
+            
+        
+            EnterCorrectly:
+            Console.WriteLine("Yeni bir kitab elave etmnk isteyirsinizmi? y/n ");
+            YorN = Console.ReadLine();
+            YorN = YorN.Trim();
+            
+            if (YorN.ToLower() == "y")
+                goto EnterAgain;
+            
+            else if (YorN.ToLower() == "n")
+            {
+                int i = 1;
+               foreach(Book item in books)
+                {
+                    Console.WriteLine($"{i}. Kitabin adi: {item.Name}\n{i}. Kitabin sehife sayi: {item.PageCount}\n=========="); ;
+                    i++;
+                } 
+            }
+
+            else
+                goto EnterCorrectly;
+
+        }
+    }
+}
